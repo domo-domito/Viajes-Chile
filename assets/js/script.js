87db194df5c2d0aa15e3fb0bfee09ee450da285a
Dominique <domibeyer@gmail.com>
2021-09-17 20:30:21 -0300

    Se añade formulario Typeform y se mejoran detalles de estilo de la página

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,26 @@ $(function(){
 
     $('[data-toggle="tooltip"]').tooltip();
 
+    $("a").click(function(event){
+        if(this.hash !== "") {
+          event.preventDefault();
+
+          var gato = this.hash;
+
+          $("html, body").animate({
+            scrollTop: $(gato).offset().top
+          }, "slow", function(){
+            window.location.hash = gato;
+          });
+        }
+    });
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 715){  
+          $('nav').addClass("bg-info");
+            } else {
+                $('nav').removeClass("bg-info");
+            }
+        });
+
 });
